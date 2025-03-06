f606f2f4af8b87d2f197a2fb96bf976dbb0f06cf
Seyedali-Hezaveh <seyedali.hezaveh29@sharif.edu>
2025-03-06 19:30:17 +0330

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# SeyedaliHezaveh.github.io
---
+++ precache-manifest.405f338ec1784e7914bc72cd7593dd42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fe1d06ad5d6c03190e68c8f731d5ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "092e273105dc1caa26ce",
+    "url": "/static/css/main.a814e88b.chunk.css"
+  },
+  {
+    "revision": "06519f1c9a8122aa0099",
+    "url": "/static/js/2.4bba8263.chunk.js"
+  },
+  {
+    "revision": "55c3ba2a477c9e4968b6ce8d4f4ebf4b",
+    "url": "/static/js/2.4bba8263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96689a34628cfcfc53b6",
+    "url": "/static/js/3.cef52eb0.chunk.js"
+  },
+  {
+    "revision": "092e273105dc1caa26ce",
+    "url": "/static/js/main.026d6f2f.chunk.js"
+  },
+  {
+    "revision": "3998120e9a270a65bb6e",
+    "url": "/static/js/runtime-main.6287294c.js"
+  }
+]);
