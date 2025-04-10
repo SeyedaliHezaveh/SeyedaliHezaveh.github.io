31042a9fe5a963d799af3f1685afeaab99db2047
Seyedali-Hezaveh <129006739+SeyedaliHezaveh@users.noreply.github.com>
2025-04-10 13:44:13 +0330

    Add files via upload

---
+++ precache-manifest.9197cbf58d86a6f644ad2d8fc73e5c0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c31b1655050cad658c93c259660a411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ed5e686c87c6e56d75d",
+    "url": "/static/css/main.76dfeb37.chunk.css"
+  },
+  {
+    "revision": "7cfc0d75af4ff12058ec",
+    "url": "/static/js/2.66fde604.chunk.js"
+  },
+  {
+    "revision": "55c3ba2a477c9e4968b6ce8d4f4ebf4b",
+    "url": "/static/js/2.66fde604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0356511b2deb512e3287",
+    "url": "/static/js/3.96771f78.chunk.js"
+  },
+  {
+    "revision": "9ed5e686c87c6e56d75d",
+    "url": "/static/js/main.97be8b6a.chunk.js"
+  },
+  {
+    "revision": "0c9bcf8b5744d010533a",
+    "url": "/static/js/runtime-main.a8c787f7.js"
+  }
+]);
